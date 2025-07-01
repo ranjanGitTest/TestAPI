550af71fe59828eddd7149e8a3088d6f596227a1
Ranjan <mondal.ranjan6@gmail.com>
2025-07-01 15:02:34 +0530

    Initial Commit

---
+++ TestAPI/TestAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAPIApp.swift
+//  TestAPI
+//
+//  Created by Ranjan on 01/07/25.
+//
+
+import SwiftUI
+
+@main
+struct TestAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
